b61461c1efcfa7aaa5f818cbb2c1ef90f1f161b3
Romulo <romulotg12@gmail.com>
2024-09-06 12:55:55 -0300

    refact: change the job (schedule) to vercel.json instead of using your own function when starting flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
-import threading
-from app.job import run_schedule
 
 app = Flask(__name__)
-
-def start_scheduler():
-    scheduler_thread = threading.Thread(target=run_schedule, daemon=True)
-    scheduler_thread.start()
-
-start_scheduler()
 
 from app import routes
---
+++ app/api/cron.py
@@ -0,0 +1,9 @@
+from app.scrap.hltv import get_upcoming_matches, save_matches_to_json
+
+def handler(req, res):
+    print("Updating Brazilian matches...")
+    matches = get_upcoming_matches()
+    save_matches_to_json(matches)
+    print("Updated matches!")
+    
+    return res.status(200).json({"message": "Matches updated successfully!"})
